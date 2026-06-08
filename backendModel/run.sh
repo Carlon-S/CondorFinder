98bfb58ef5978d8466699160dc379d3dc0046a69
Bastian Kretschmer <54518276+Bartianwar@users.noreply.github.com>
2026-06-08 18:54:05 -0400

    Update run.sh

--- backendModel/run.sh
+++ backendModel/run.sh
@@ -10,9 +10,6 @@ source .venv/bin/activate
 
 FILENAME=$(python3 joining/joinOrtho.py "$1")
 
-python3 detecting/detectingOrtho.py "joining/finals/$FILENAME"
+echo "Joining done. File: $FILENAME"
 
-if [[ -z "$FILENAME" ]]; then
-    echo "Error: No se encontro archivo"
-    exit 1
-fi
+python3 detecting/detectingOrtho.py --input "joining/finals/$FILENAME"
